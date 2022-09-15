4c3254a7fef9ea04e2c6a0864d1982e31f6475b1
dey_gu <87346972+degu0@users.noreply.github.com>
2022-09-15 18:21:06 -0300

    Create Horario.php

---
+++ src/view/tabela/Horario.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/librabres/css/horario.css">
+    <script src="/librabres/js/DragDrop.js" defer></script>
+    <title>Schorn - Horarios</title>
+</head>
+
+<body>
+    <a href="/">HOME</a>
+    <h1>Tabela horário</h1>
+    <div class="board">
+            <div class="dropzone">
+                <div class="card" draggable="true">
+                    <div class="content">MATHEUS</div>
+                </div>
+            </div>
+        </div>
+        <div class="board">
+            <h3>1 ANO</h3>
+            <div class="dropzone">
+            </div>
+            <h4>mkt a</h4>
+            <div class="dropzone">
+            </div>
+        </div>
+</body>
